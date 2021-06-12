76dfbe3a916602e53cf17694f1a4278b785a7383
Salman Ameer <59564333+SalmanAmeer@users.noreply.github.com>
2021-06-12 08:30:49 -0700

    Add files via upload

---
+++ ShutDown Render.jsx
@@ -0,0 +1,47 @@
+﻿// Main Window
+var win = new Window("palette",undefined);
+win.orientation = "column";
+
+// Group 1 for Adding a Buttons or Elements
+var Grp1 = win.add("group",undefined);
+Grp1.orientation = "row";
+
+//Button
+var ButtonRender = Grp1.add("button",undefined);
+ButtonRender.text =  "Rendown"; //Render + Shutdown Button
+
+
+//Show the Window & Center its position
+win.show();
+win.center();
+
+ButtonRender.onClick = function(){
+        if(app.project.renderQueue.numItems<1){
+            alert("Please Add Comps for Render");
+            }else{
+            app.project.renderQueue.render(); // Start Rendering
+            app.executeCommand(5); //Save Project after Rendering
+            ShutdownPC();
+            };
+    };
+
+//Shutdown Function
+function ShutdownPC(){
+    
+    var ShutdownCode = "c:\\windows\\system32\\shutdown -s -f -t 01";
+    var filepass = new File(new File($.fileName).parent);
+	var file0 = filepass.fullName;
+    var file1 = file0 + "/";
+    var file = File(file1 + "SD.bat");
+    
+    if(!file.exists){
+        file.open("w");
+        file.write(ShutdownCode);
+        file.close();
+        file.execute();
+        }else{
+            file.execute();
+            };
+    
+    
+    };
